c9f41bc47bafe1ba1efa500818d029aa2c96ad21
Chetanjain10 <chetanjainsrs@gmail.com>
2021-07-08 17:34:27 +0530

    usestate_

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './UseState/Us';
 import Main from './ContextAPI/Main';
+import Us from './UseState/Us1';
 function App() {
   return (
-    <Main />
+    <Us />
   );
 }
 
--- src/UseState/Us1.js
+++ src/UseState/Us1.js
@@ -2,12 +2,16 @@ import React, {useState} from 'react'
 
 function Us(){
     const[msgObj,setMsg]=useState({message:''});
-    const handleChange=()=>{
-        
+    const handleChange=(e)=>{
+        let val = e.target.val;
+        // msgObj.message=val;
+        let obj = {message:val}
+        setMsg(obj);
     }
     return (
         <div>
         <input type="text" value={msgObj.message} onChange={handleChange}></input>
+        <p>{msgObj.message}</p>
         </div>
         )
 }
